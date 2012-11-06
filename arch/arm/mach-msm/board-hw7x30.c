70a2e1605c8757bf7fd72214fa4afbbb37e70b40
Martin Johnson <M.J.Johnson@massey.ac.nz>
2012-11-07 09:40:25 +1300

    change wifi sd card speed to 40MHz

--- arch/arm/mach-msm/board-hw7x30.c
+++ arch/arm/mach-msm/board-hw7x30.c
@@ -8529,7 +8529,7 @@ static struct mmc_platform_data msm7x30_sdc3_data = {
 #endif
 	.msmsdcc_fmin	= 144000,
 	.msmsdcc_fmid	= 24576000,
-	.msmsdcc_fmax	= 49152000,
+	.msmsdcc_fmax	= 40960000,
 	.nonremovable	= 0,
 };
 #endif
